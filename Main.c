78213e44110eb16a79348049f328d17226327c1d
Edgar Perez <edgar.a.perez@wsu.edu>
2016-10-02 14:22:43 -0700

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Main.c
+ *
+ *    Description:  This is a simple "Hello World" program.
+ *
+ *        Version:  1.0
+ *        Created:  8/27/2016 3:10:56 PM
+ *       Revision:  none
+ *       Compiler:  msft c
+ *
+ *         Author:  Edgar Perez 
+ *   Organization:  Washington State University
+ *  Collaborators:  Jasdeep Singh
+ *
+ * =====================================================================================
+ */
+
+#include	<stdio.h>
+
+/* Outputs "Hello World" */
+int main(void)
+{
+	printf("Hello World! This is CptS121!");
+
+	return 0;
+}
